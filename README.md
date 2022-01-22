5ed3bab8fe55818591c226467fa98b3b948dbed2
Diogo Xavier <diogoxavierpinto@gmail.com>
2022-01-22 17:19:52 +0000

    Add nix-based automatic setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# Monorepo
+
+## Setup
+
+This repo leverages Nix to install all dependencies automatically.
+
+Then only dependencies we need to manually install are:
+* [Nix](https://github.com/NixOS/nix)
+* [direnv](https://github.com/direnv/direnv)
+
+Once they're installed, we just need to run:
+```sh
+direnv allow
+```
+
+And we're all done! :tada:
---
+++ nix/shell.nix
@@ -0,0 +1,29 @@
+{ system ? builtins.currentSystem, nixpkgs ? import <nixpkgs> {} }:
+let
+  pkgs = import <nixpkgs> {
+    inherit system; config = { };
+    overlays = [
+      (self: super: 
+        let
+          sources = super.callPackages ./_sources/generated.nix { };
+        in
+        {
+          nvfetcher = super.nvfetcher.overrideAttrs (old: {
+            inherit (sources.nvfetcher) src version;
+          });
+          golangci-lint = super.golangci-lint.overrideAttrs (old: {
+            inherit (sources.golangci-lint) src version;
+          });
+          frugal = super.frugal.overrideAttrs (old: {
+            inherit (sources.frugal) src version;
+          });
+        }
+      )
+    ];
+  };
+  src = fetchTarball "https://github.com/numtide/devshell/archive/master.tar.gz";
+  devshell = import src { 
+    inherit pkgs system;
+  };
+in
+devshell.fromTOML ./devshell.toml
